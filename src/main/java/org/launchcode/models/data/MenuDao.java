7dec740923dd1e3051991938ece8f8a07fc849ba
e-inquirer <sorour.a@gmail.com>
2017-04-13 12:38:57 -0400

    created `MenuDao` interface | enables Spring Data to store and retrieve instances of the `Menu` class

---
+++ src/main/java/org/launchcode/models/data/MenuDao.java
@@ -0,0 +1,17 @@
+package org.launchcode.models.data;
+
+import org.launchcode.models.Category;
+import org.launchcode.models.Menu;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+/**
+ * Created by ahmed on 4/13/17.
+ */
+
+@Repository
+@Transactional
+public interface MenuDao extends CrudRepository<Menu, Integer> {
+}
