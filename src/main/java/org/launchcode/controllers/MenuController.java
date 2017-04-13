4738362b6ffde1cb759f0da4bc64835182335231
e-inquirer <sorour.a@gmail.com>
2017-04-13 13:25:36 -0400

    created `add` GET handler method

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -1,11 +1,13 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Menu;
 import org.launchcode.models.data.CheeseDao;
 import org.launchcode.models.data.MenuDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by ahmed on 4/13/17.
@@ -29,5 +31,16 @@ public class MenuController {
 
         return "menu/index";
     }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddMenuForm(Model model) {
+
+        model.addAttribute(new Menu());
+        model.addAttribute("title", "Add Menu");
+
+        return "menu/add";
+    }
+
+
 }
 
