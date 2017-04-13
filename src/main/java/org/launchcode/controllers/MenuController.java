c3d02297cc935d2ce21c5d01cf0d7f7cc8685f78
e-inquirer <sorour.a@gmail.com>
2017-04-13 13:44:47 -0400

    created `add` POST handler method

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -6,8 +6,12 @@ import org.launchcode.models.data.MenuDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 /**
  * Created by ahmed on 4/13/17.
@@ -41,6 +45,18 @@ public class MenuController {
         return "menu/add";
     }
 
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddMenuForm(Model model,
+                                     @ModelAttribute @Valid Menu menu,
+                                     Errors errors) {
 
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Menu");
+            return "menu/add";
+        }
+
+        menuDao.save(menu);
+        return "redirect:view/" + menu.getId();
+    }
 }
 
