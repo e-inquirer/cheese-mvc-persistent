a42951521d9e364454a3b5c6fd25eac4c0c0426d
e-inquirer <sorour.a@gmail.com>
2017-04-13 02:14:06 -0400

    added controller handlers to render and process the form

--- src/main/java/org/launchcode/controllers/CategoryController.java
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -1,10 +1,16 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Category;
 import org.launchcode.models.data.CategoryDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 /**
  * Created by ahmed on 4/13/17.
@@ -26,4 +32,23 @@ public class CategoryController {
         return "category/index";
     }
 
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model) {
+
+        model.addAttribute("title", "Category");
+        model.addAttribute("category", new Category());
+        return "category/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model model,
+                      @ModelAttribute @Valid Category category, Errors errors) {
+
+        if (errors.hasErrors()) {
+            return "category/add";
+        }
+        categoryDao.save(category);
+        return "redirect:";
+    }
 }
+
