8bac0f3504f07dcc95cbf8cad1becdf59e65b1dd
e-inquirer <sorour.a@gmail.com>
2017-04-13 13:50:58 -0400

    created `viewMenu` GET handler

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -57,6 +58,16 @@ public class MenuController {
 
         menuDao.save(menu);
         return "redirect:view/" + menu.getId();
+    }
+
+    @RequestMapping(value = "view/{id}", method = RequestMethod.GET)
+    public String viewMenu(@PathVariable int id, Model model) {
+        Menu menu = menuDao.findOne(id);
+
+        model.addAttribute("menu", menu);
+        model.addAttribute("title", menu.getName());
+
+        return "menu/view";
     }
 }
 
